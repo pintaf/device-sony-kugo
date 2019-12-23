fdc1cdfcdaf5dea9dc0b601726c04b38df752b5f
Loïc Poisot <elpoisot@hotmail.fr>
2019-12-23 21:33:52 +0100

    first addaptations for lineage

--- lineage_kugo.mk
+++ lineage_kugo.mk
@@ -1,4 +1,3 @@
-#
 # Copyright 2014 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,10 +11,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-add_lunch_combo aosp_f5321-eng
-add_lunch_combo aosp_f5321-userdebug
-add_lunch_combo lineage_kugo-eng
-add_lunch_combo lineage_kugo-userdebug
+TARGET_KERNEL_CONFIG := aosp_loire_kugo_defconfig
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/sony/kugo/device.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+PRODUCT_NAME := lineage_kugo
+PRODUCT_DEVICE := kugo
+PRODUCT_MODEL := Xperia X Compact
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
